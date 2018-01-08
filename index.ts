9b6412a58dec67dcd1507902fc8c7dc1ce9990b9
Jesse Silber <jessesilber@gmail.com>
2018-01-08 16:23:08 -0500

    moved index to root

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Configuration } from './src/Configuration';
+import { Recipient } from './src/Recipient';
+import { RecipientAccount } from './src/RecipientAccount';
+import { Balances } from './src/Balances';
+import { Batch } from './src/Batch';
+import { Payment } from './src/Payment';
+
+module.exports = {
+    Configuration,
+    Recipient,
+    RecipientAccount,
+    Balances,
+    Payment,
+    Batch,
+};
