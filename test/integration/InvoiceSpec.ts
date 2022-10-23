29f99c5f40a7067ba8f62d1bd8f3f8093b968116
Zero <zero.bites.inbox@gmail.com>
2022-10-22 19:07:29 -0600

    wip - struggling to run matchers

--- test/integration/InvoiceSpec.ts
+++ test/integration/InvoiceSpec.ts
@@ -8,78 +8,80 @@ const invoiceFactory = new InvoiceFactory();
 const recipientFactory = new RecipientFactory();
 
 describe("Invoice", () => {
-    it("creates an invoice", async () => {
-        const nockDone = await startNockRec('invoice-create.json');
+    // it("creates an invoice", async () => {
+    //     const nockDone = await startNockRec('invoice-create.json');
+    //
+    //     const recipient = await recipientFactory.createResource();
+    //
+    //     const invoice = await invoiceFactory.createResource({
+    //         recipientId: recipient.id
+    //     });
+    //
+    //     assert.ok(invoice);
+    //     assert.strictEqual("testInvoice", invoice.externalId);
+    //     assert.strictEqual(recipient.id, invoice.recipientId);
+    //
+    //     nockDone();
+    // });
 
-        const recipient = await recipientFactory.createResource();
+    // it("finds an invoice", async () => {
+    //     const nockDone = await startNockRec('invoice-find.json');
+    //
+    //     const recipient = await recipientFactory.createResource();
+    //
+    //     const invoice = await invoiceFactory.createResource({
+    //         recipientId: recipient.id
+    //     });
+    //
+    //     const findInvoice = await testingApiClient.invoice.find(invoice.id);
+    //
+    //     assert.ok(invoice);
+    //     assert.strictEqual("testInvoice", invoice.externalId);
+    //
+    //     nockDone();
+    // });
 
-        const invoice = await invoiceFactory.createResource({
-            recipientId: recipient.id
-        });
+    // it("updates an invoice", async () => {
+    //     const nockDone = await startNockRec('invoice-update.json');
+    //
+    //     const recipient = await recipientFactory.createResource();
+    //
+    //     const invoice = await invoiceFactory.createResource({
+    //         recipientId: recipient.id
+    //     });
+    //
+    //     const updatedInvoice = await testingApiClient.invoice.update(
+    //         invoice.id,
+    //         {
+    //             ...invoiceFactory.defaultAttrs,
+    //             description: "Updated description",
+    //             recipientId: recipient.id,
+    //             lines: [],
+    //         });
+    //
+    //     assert.ok(invoice);
+    //     assert.strictEqual("Updated description", updatedInvoice.description);
+    //
+    //     nockDone();
+    // });
 
-        assert.ok(invoice);
-        assert.strictEqual("testInvoice", invoice.externalId);
-        assert.strictEqual(recipient.id, invoice.recipientId);
-
-        nockDone();
-    });
-
-    it("finds an invoice", async () => {
-        const nockDone = await startNockRec('invoice-find.json');
-
-        const recipient = await recipientFactory.createResource();
-
-        const invoice = await invoiceFactory.createResource({
-            recipientId: recipient.id
-        });
-
-        const findInvoice = await testingApiClient.invoice.find(invoice.id);
-
-        assert.ok(invoice);
-        assert.strictEqual("testInvoice", invoice.externalId);
-
-        nockDone();
-    });
-
-    it("updates an invoice", async () => {
-        const nockDone = await startNockRec('invoice-update.json');
-
-        const recipient = await recipientFactory.createResource();
-
-        const invoice = await invoiceFactory.createResource({
-            recipientId: recipient.id
-        });
-
-        const updatedInvoice = await testingApiClient.invoice.update(
-            invoice.id,
-            {
-                ...invoiceFactory.defaultAttrs,
-                description: "Updated description",
-                recipientId: recipient.id,
-                lines: [],
-            });
-
-        assert.ok(invoice);
-        assert.strictEqual("Updated description", updatedInvoice.description);
-
-        nockDone();
-    });
-
-    it("deletes an invoice", async () => {
-        const nockDone = await startNockRec('invoice-delete.json');
-
-        const recipient = await recipientFactory.createResource();
-
-        const invoice = await invoiceFactory.createResource({
-            recipientId: recipient.id
-        });
-
-        assert.ok(testingApiClient.invoice.remove(invoice.id));
-
-        nockDone();
-    });
+    // it("deletes an invoice", async () => {
+    //     const nockDone = await startNockRec('invoice-delete.json');
+    //
+    //     const recipient = await recipientFactory.createResource();
+    //
+    //     const invoice = await invoiceFactory.createResource({
+    //         recipientId: recipient.id
+    //     });
+    //
+    //     assert.ok(testingApiClient.invoice.remove(invoice.id));
+    //
+    //     nockDone();
+    // });
 
     it("searches for an invoice", async () => {
+        expect(1).toBe(1);
+
         const nockDone = await startNockRec('invoice-search.json');
 
         const recipient = await recipientFactory.createResource();
