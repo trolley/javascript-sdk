64c0ffe3bda4c0accc84baf24dc0d0fae03c174e
Zero <zero.bites.inbox@gmail.com>
2022-10-25 21:11:11 -0600

    wip payment spec

---
+++ test/integration/PaymentSpec.ts
@@ -0,0 +1,98 @@
+import { startNockRec, testingApiClient } from "./helpers/integrationTestsHelpers";
+import { PaymentFactory } from "./factories/PaymentFactory";
+import { BatchFactory } from "./factories/BatchFactory";
+import { RecipientFactory } from "./factories/RecipientFactory";
+import * as assert from "assert";
+
+let batchFactory: BatchFactory;
+let recipientFactory: RecipientFactory;
+let paymentFactory: PaymentFactory;
+
+before(async () => {
+    batchFactory = new BatchFactory();
+    recipientFactory = new RecipientFactory();
+    paymentFactory = new PaymentFactory();
+});
+
+describe('Payment', () => {
+    it('creates a payment', async () => {
+        const nockDone = await startNockRec('payment-create.json');
+
+        const recipient = await recipientFactory.createResource();
+        const batch = await batchFactory.createResource();
+        const payment = await paymentFactory.createResource(
+            {
+                batch: {
+                    id: batch.id,
+                },
+                payment: {
+                    recipient: {
+                        id: recipient.id,
+                    },
+                },
+            },
+        );
+
+        nockDone();
+
+        assert.ok(payment);
+        assert.strictEqual('testPayment', payment.externalId);
+    });
+
+    it('updates a payment', async () => {
+        const nockDone = await startNockRec('payment-update.json');
+
+        const recipient = await recipientFactory.createResource();
+        const batch = await batchFactory.createResource();
+        const payment = await paymentFactory.createResource(
+            {
+                batch: {
+                    id: batch.id,
+                },
+                payment: {
+                    recipient: {
+                        id: recipient.id,
+                    },
+                },
+            },
+        );
+        const updatedPayment = await testingApiClient.payment.update(
+            payment.id,
+            batch.id,
+            {
+                paymentId: payment.id,
+                externalId: 'updatedPayment',
+            },
+        );
+
+        nockDone();
+
+        assert.ok(updatedPayment);
+        assert.strictEqual('updatedPayment', updatedPayment.externalId);
+    });
+
+    it('deletes a payment', async () => {
+        const nockDone = await startNockRec('payment-delete.json');
+
+        const recipient = await recipientFactory.createResource();
+        const batch = await batchFactory.createResource();
+        const payment = await paymentFactory.createResource(
+            {
+                batch: {
+                    id: batch.id,
+                },
+                payment: {
+                    recipient: {
+                        id: recipient.id,
+                    },
+                },
+            },
+        );
+        const deletedPaymentResult = await testingApiClient.payment.remove(payment.id, batch.id);
+
+        nockDone();
+
+        assert.ok(deletedPaymentResult);
+        assert.strictEqual(true, deletedPaymentResult);
+    });
+});
