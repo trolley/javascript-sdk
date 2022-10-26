95317988ab62b82d4abde51cd61f5dc0e6290473
Zero <zero.bites.inbox@gmail.com>
2022-10-26 09:38:24 -0600

    style batch

--- test/integration/BatchSpec.ts
+++ test/integration/BatchSpec.ts
@@ -1,16 +1,23 @@
 import * as assert from "assert";
-import {testingApiClient, startNockRec} from "./helpers/integrationTestsHelpers";
-import {BatchFactory} from "./factories/BatchFactory";
-import {RecipientFactory} from "./factories/RecipientFactory";
-import {RecipientAccountFactory} from "./factories/RecipientAccountFactory";
+import { testingApiClient, startNockRec } from "./helpers/integrationTestsHelpers";
+import { BatchFactory } from "./factories/BatchFactory";
+import { RecipientFactory } from "./factories/RecipientFactory";
+import { RecipientAccountFactory } from "./factories/RecipientAccountFactory";
 
-const batchFactory = new BatchFactory();
-const recipientFactory = new RecipientFactory();
-const recipientAccountFactory = new RecipientAccountFactory();
+let batchFactory: BatchFactory;
+let recipientFactory: RecipientFactory;
+let recipientAccountFactory: RecipientAccountFactory;
+
+before(async () => {
+    batchFactory = new BatchFactory();
+    recipientFactory = new RecipientFactory();
+    recipientAccountFactory = new RecipientAccountFactory();
+});
 
 async function createRecipient(email: string) {
    const recipient = await recipientFactory.createResource({ email });
    await recipientAccountFactory.createResource({ recipient });
+
    return recipient;
  }
 
@@ -21,12 +28,12 @@ describe("Batch", () => {
     const batch = await batchFactory.createResource();
     const all = await testingApiClient.batch.all();
 
+    nockDone();
+
     assert.ok(batch);
     assert.ok(batch.id);
     assert.ok(all.length > 0);
-
-    nockDone();
-  })
+  });
 
   it("updates a batch", async () => {
     const nockDone = await startNockRec('batch-update.json');
@@ -40,16 +47,16 @@ describe("Batch", () => {
       description: "Integration Test Update 2",
     });
     batch = await testingApiClient.batch.find(batch.id);
+
+    nockDone();
+
     assert.ok(batch);
     assert.strictEqual(batch.description, "Integration Test Update 2");
     assert.strictEqual(batch.status, "open");
+  });
 
-    nockDone();
-  })
-
-  //tslint:disable-next-line:mocha-no-side-effect-code
   it("create with payments", async () => {
-    const nockDone = await startNockRec('batch-create-with-payments.json')
+    const nockDone = await startNockRec('batch-create-with-payments.json');
 
     const recipient = await createRecipient('recipient.with.payment@example.com');
     const otherRecipient = await createRecipient('recipient.with.payment.2@example.com');
@@ -65,59 +72,57 @@ describe("Batch", () => {
           sourceAmount: "10.00",
           recipient: { id: otherRecipient.id },
         },
-      ]
+      ],
     });
+
+    const findBatch = await testingApiClient.batch.find(batch.id);
+
+    nockDone();
 
     assert.ok(batch);
     assert.ok(batch.id);
-    const findBatch = await testingApiClient.batch.find(batch.id);
-
     assert.ok(findBatch);
     assert.strictEqual(batch.totalPayments, 2);
 
-    let payments = await testingApiClient.batch.paymentList(batch.id);
+    const payments = await testingApiClient.batch.paymentList(batch.id);
     for (const item of payments) {
       assert.strictEqual(item.status, "pending");
     }
-
-    nockDone();
   });
 
-  //tslint:disable-next-line:mocha-no-side-effect-code
   it("starts processing batch payments", async () => {
-    const nockDone = await startNockRec('batch-processing-payments.json')
+    const nockDone = await startNockRec('batch-processing-payments.json');
 
     const recipient = await createRecipient('processing.recipient@example.com');
-    const other_recipient = await createRecipient('processing.recipient2@example.com');
+    const otherRecipient = await createRecipient('processing.recipient2@example.com');
 
     const batch = await batchFactory.createResource({
       payments: [
         {
           targetAmount: "10.00",
           targetCurrency: "EUR",
-          recipient: {id: recipient.id},
+          recipient: { id: recipient.id },
         },
         {
           sourceAmount: "10.00",
-          recipient: {id: other_recipient.id},
+          recipient: { id: otherRecipient.id },
         },
-      ]
+      ],
     });
 
-    assert.ok(batch);
-    assert.ok(batch.id);
-
     const summary = await testingApiClient.batch.summary(batch.id);
-    assert.strictEqual(2, summary.detail["bank-transfer"].count, "Bad Count");
-
     const quote = await testingApiClient.batch.generateQuote(batch.id);
-    assert.ok(quote, "failed to get quote");
 
     // There's an issue here when it runs too quick. It returns "Operation In Progress"
     // Sleep when running against real API
     const start = await testingApiClient.batch.startProcessing(batch.id);
-    assert.ok(start, "Failed to start");
 
     nockDone();
+
+    assert.ok(batch);
+    assert.ok(batch.id);
+    assert.strictEqual(2, summary.detail["bank-transfer"].count, "Bad Count");
+    assert.ok(quote, "failed to get quote");
+    assert.ok(start, "Failed to start");
   });
 });
