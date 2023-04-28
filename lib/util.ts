c7e351f4ec25366ef0b42c1f43c99b4d82a065ba
Zero <zero.bites.inbox@gmail.com>
2023-04-28 01:03:05 -0600

    initial change

--- lib/util.ts
+++ lib/util.ts
@@ -8,3 +8,18 @@ export function buildURL(base: string, ...parts: string[]) {
   return ['', 'v1'].concat([base], parts.map(encodeURIComponent))
     .join('/');
 }
+
+interface PaginationMeta {
+  page: number;
+  pages: number;
+  records: number;
+}
+
+export class PaginatedArray<T> extends Array<T> {
+  meta: PaginationMeta;
+
+  constructor(meta: PaginationMeta, ...items: T[]) {
+    super(...items);
+    this.meta = meta;
+  }
+}
